7b82a8753b7b1bbe16e8fafa333c7c96746ac9be
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2021-06-01 01:26:26 +0530

    Endpoint for the program created

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 from flask import Flask
 
 app = Flask(__name__)
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'item': [
+            {
+                'name': 'My Item',
+                'price': '15.99'
+            }
+        ]
+
+    }
+]
+
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/store')
+def get_store():
+    pass
+
+
+@app.route('/store/<string:name>/item', method=['POST'])
+def create_item_in_store(name):
+    pass
 
 
 @app.route('/')
