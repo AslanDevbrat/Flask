b189383285e92955f38c4ce327e5118f2f8079e3
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2021-06-01 18:51:30 +0530

    Loging and authentication done

--- section4/app.py
+++ section4/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'admin'
 api = Api(app)
+
+jwt = JWT(app,authenticate, identity)
 
 items = [
 
@@ -10,6 +15,7 @@ items = [
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
 
