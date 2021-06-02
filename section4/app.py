d67ade398871db3cb533f35245162a5e68821c4c
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2021-06-02 14:38:59 +0530

    Completed PUT

--- section4/app.py
+++ section4/app.py
@@ -31,6 +31,20 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {"message":"item deleted"}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name,items), None)
+        if item is None:
+            item = {'name':name, 'price':data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
