32dae53055a58b520950de4d6485f902ee11942e
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2021-06-02 16:30:52 +0530

    Completed Section 4

--- section4/app.py
+++ section4/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = 'admin'
 api = Api(app)
 
-jwt = JWT(app,authenticate, identity)
+jwt = JWT(app, authenticate, identity)
 
 items = [
 
@@ -15,6 +15,12 @@ items = [
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!")
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -23,10 +29,11 @@ class Item(Resource):
                 }, 200 if item else 400
 
     def post(self, name):
-        if  next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message':'An item of {} name already exists'.format(name)}, 400
-        data = request.get_json()
 
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': 'An item of {} name already exists'.format(name)}, 400
+        #data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -34,17 +41,19 @@ class Item(Resource):
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
-        return {"message":"item deleted"}
+        return {"message": "item deleted"}
 
     def put(self, name):
-        data = request.get_json()
-        item = next(filter(lambda x: x['name'] == name,items), None)
+        data = Item.parser.parse_args()
+        print(data['another'])
+        item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
-            item = {'name':name, 'price':data['price']}
+            item = {'name': name, 'price': data['price']}
             items.append(item)
         else:
             item.update(data)
         return item
+
 
 class ItemList(Resource):
     def get(self):
